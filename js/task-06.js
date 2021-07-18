41a6fe10ff21c06ae729d88d863b1ad048423c7e
Eugenia Kolodeichuk <kolodeichuk.e@gmail.com>
2021-07-18 15:07:10 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,25 @@
-const inputEl = document.querySelector("#validation-input");
+const inputEl = document.querySelector('#validation-input');
+
+/* const validEl = function (event) {
+    if (event.curretTarget.value.length === Number(inputEl.dataset.length)) {
+        inputEl.classList.add('valid');
+        inputEl.classList.remove('invalid');
+    } else {
+        inputEl.classList.add('invalid');
+        inputEl.classList.remove('valid');
+    }
+}
+
+inputEl.addEventListener('blur', validEl); */
 
 
 inputEl.addEventListener('change', (event) => {
     if (Number(inputEl.dataset.length) !== inputEl.value.length) {
         inputEl.classList.add('invalid');
+        inputEl.classList.remove('valid');
     }
     else {
-        inputEl.classList.replace('invalid', 'valid');
+        inputEl.classList.add('valid');
+        inputEl.classList.remove('invalid');
     }
 });
